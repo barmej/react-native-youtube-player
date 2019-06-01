e2306267a7ecc7951de06cfc269d321ba4a97bbf
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-06-01 17:57:18 +0000

    fix(build): inline scripts

--- example/App.js
+++ example/App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-import YoutubePlayer from "react-native-youtube-player";
+import YoutubePlayer from "react-native-yt-player";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
