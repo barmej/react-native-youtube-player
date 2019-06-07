2d17436eb2a4638ed579aeb75a891a3b7ef8a167
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-06-07 02:13:45 +0000

    feat(player): fix android build

--- src/mobile/Player.tsx
+++ src/mobile/Player.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Animated, { Easing } from "react-native-reanimated";
 const { Value, timing, concat } = Animated;
 
