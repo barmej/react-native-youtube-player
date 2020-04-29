e515ddc8faa9ea4500112889a64143eeb06d04b0
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-04-29 01:26:54 +0000

    feat(player): generate new example using RN62

---
+++ example/App.tsx
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+import YoutubePlayer from 'react-native-yt-player';
+
+const TopBar = () => (
+  <View
+    style={{
+      alignSelf: 'center',
+      position: 'absolute',
+      top: 0,
+    }}>
+    <Text style={{color: '#FFF'}}> Custom Top bar</Text>
+  </View>
+);
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={{paddingTop: 60}}>
+          <YoutubePlayer
+            topBar={TopBar}
+            videoId="Z1LmpiIGYNs"
+            autoPlay
+            //onFullScreen={this.onFullScreen}
+            onStart={() => console.log('onStart')}
+            onEnd={() => console.log('on End')}
+          />
+
+          <View>
+            <Text>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+              aspernatur rerum, deserunt cumque ipsam unde nam voluptatum
+              tenetur cupiditate veritatis autem quidem ad repudiandae sapiente
+              odit voluptates fugit placeat ut!
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
--- example/metro.config.js
+++ example/metro.config.js
@@ -1,21 +1,44 @@
-const path = require("path");
-const blacklist = require("metro-config/src/defaults/blacklist");
-const escape = require("escape-string-regexp");
-const pak = require("../package.json");
+/* eslint-disable import/no-extraneous-dependencies */
 
-const peerDependencies = Object.keys(pak.peerDependencies);
+const path = require('path');
+const fs = require('fs');
+const blacklist = require('metro-config/src/defaults/blacklist');
+const escape = require('escape-string-regexp');
+
+const root = path.resolve(__dirname, '..');
+const pak = JSON.parse(
+  fs.readFileSync(path.join(root, 'package.json'), 'utf8'),
+);
+
+const modules = [
+  '@babel/runtime',
+  ...Object.keys({
+    ...pak.dependencies,
+    ...pak.peerDependencies,
+  }),
+];
 
 module.exports = {
   projectRoot: __dirname,
-  watchFolders: [path.resolve(__dirname, "..")],
+  watchFolders: [root],
 
   resolver: {
     blacklistRE: blacklist([
-      new RegExp(
-        `^${escape(path.resolve(__dirname, "..", "node_modules"))}\\/.*$`
-      )
+      new RegExp(`^${escape(path.join(root, 'node_modules'))}\\/.*$`),
     ]),
 
-    providesModuleNodeModules: ["@babel/runtime", ...peerDependencies]
-  }
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
+  },
+
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
 };
