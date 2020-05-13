e2bcee1dc079e2b03de15c20a1c1d0b6ba78b517
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-05-13 00:28:03 +0000

    fix(example): update example to latest RN version

--- example/App.js
+++ example/App.js
@@ -6,41 +6,40 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import YoutubePlayer from "react-native-yt-player";
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import YoutubePlayer from 'react-native-yt-player';
 
-const TopBar = ({ play, fullScreen }) => (
+const TopBar = ({play, fullScreen}) => (
   <View
     style={{
-      alignSelf: "center",
-      position: "absolute",
-      top: 0
-    }}
-  >
-    {!fullScreen && <Text style={{ color: "#FFF" }}> Custom Top bar</Text>}
+      alignSelf: 'center',
+      position: 'absolute',
+      top: 0,
+    }}>
+    {!fullScreen && <Text style={{color: '#FFF'}}> Custom Top bar</Text>}
   </View>
 );
 
 export default class App extends Component<Props> {
   state = {
-    fullScreen: false
+    fullScreen: false,
   };
 
-  onFullScreen = fullScreen => {
-    console.log("fullscreen ", fullScreen);
-    this.setState({ fullScreen });
+  onFullScreen = (fullScreen) => {
+    console.log('fullscreen ', fullScreen);
+    this.setState({fullScreen});
   };
   render() {
     return (
-      <View style={{ paddingTop: 60 }}>
+      <View style={{paddingTop: 60}}>
         <YoutubePlayer
           topBar={TopBar}
           videoId="Z1LmpiIGYNs"
           autoPlay
           onFullScreen={this.onFullScreen}
-          onStart={() => console.log("onStart")}
-          onEnd={() => alert("on End")}
+          onStart={() => console.log('onStart')}
+          onEnd={() => alert('on End')}
         />
 
         <View>
@@ -59,18 +58,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
+    textAlign: 'center',
+    margin: 10,
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
--- example/babel.config.js
+++ example/babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      "module-resolver",
+      'module-resolver',
       {
         alias: {
-          "react-native-yt-player": "../src/mobile"
-        }
-      }
-    ]
-  ]
+          'react-native-yt-player': '../src/mobile',
+        },
+      },
+    ],
+  ],
 };
